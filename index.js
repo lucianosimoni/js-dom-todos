dd679a2d3fd219fc7078aee9cea4174096aabf17
wagnerAJS221 <wagneralex221@gmai.com>
2022-10-27 16:41:09 +0100

    delete button added

--- index.js
+++ index.js
@@ -15,48 +15,71 @@ function getTodoList() {
       return response.json()
     })
     .then((wholeList) => {
-        state.todos = wholeList
-        renderTodo()
+      state.todos = wholeList
+      renderTodo()
     })
 }
 
 // ADD A NEW TODO BASE ON THE INPUT
 function addTodo() {
-    const newTodoData = {
-        "title": titleInput.value,
-        "completed": false
-    }
-    const uri = "http://localhost:3000/todos"
-    const options = {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        // Converts new todo data from Object to JSON
-        body: JSON.stringify(newTodoData)
-    }
+  const newTodoData = {
+    title: titleInput.value,
+    completed: false
+  }
+  const uri = 'http://localhost:3000/todos'
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    // Converts new todo data from Object to JSON
+    body: JSON.stringify(newTodoData)
+  }
 
-    fetch(uri, options)
+  fetch(uri, options)
     .then((response) => {
-        return response.json()
+      return response.json()
     })
     .then((newTodo) => {
-        state.todos.push(newTodo)
-        renderUsers();
+      state.todos.push(newTodo)
+      renderTodo()
     })
-
-
 }
 
+// DELETE A TODO BASE OFF THE LIST
+function deleteToDo(todo) {
+  const uri = `http://localhost:3000/todos/${todo.id}`
+  const options = {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+    // delete todo data from Object to JSON
+  }
 
-
+  fetch(uri, options)
+    .then((response) => {
+      return response.json()
+    })
+    .then((newTodo) => {
+      getTodoList()
+    })
+}
 
 function renderTodo() {
-    state.todos.forEach((todo) => {
-        const li = document.createElement('li')
-        li.innerText = todo.title
-        todoList.appendChild(li)
+  todoList.innerHTML = ''
+  state.todos.forEach((todo) => {
+    const li = document.createElement('li')
+    li.innerText = todo.title
+    todoList.appendChild(li)
+    const deleteButton = document.createElement('button')
+    deleteButton.innerText = 'Delete if finished'
+    todoList.appendChild(deleteButton)
+    deleteButton.addEventListener('click', (event) => {
+      event.preventDefault()
+      deleteToDo(todo)
     })
+  })
 }
 
 getTodoList()
