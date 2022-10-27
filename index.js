94936de506b284c6d5ea08040165f0cef6b1a3a5
wagnerAJS221 <wagneralex221@gmai.com>
2022-10-27 15:41:58 +0100

    console.logs added

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
-const todoList = document.querySelector("#todo-list")
+const todoList = document.querySelector('#todo-list')
 
 const state = {
-    todos: []
+  todos: []
 }
 
 // GET ALL THE TODO
 function getTodoList() {
-    const uri = "http://localhost:3000/todos"
-
-    // Get the Todo from Local Server
-    fetch(uri)
+  const uri = 'http://localhost:3000/todos'
+  console.log('test 2')
+  // Get the Todo from Local Server
+  fetch(uri)
     .then((response) => {
-        return response.json()
+      return response.json()
     })
     .then((wholeList) => {
-        state.todos = wholeList
+      state.todos = wholeList
     })
 }
 
 function renderTodoList() {
-    getTodoList()
+  console.log('test')
+  getTodoList()
 
-    const li = document.createElement("li")
-
-    state.todos.forEach((item) => {
-        console.log(item)
-    })
-
+  const li = document.createElement('li')
+  console.log(state)
+  state.todos.forEach((item) => {
+    console.log(item, 'hello5')
+  })
 }
 
 renderTodoList()
